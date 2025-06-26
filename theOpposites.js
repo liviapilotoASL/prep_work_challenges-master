478ee63b8c8eeeb39d465b7f1a63a4bfc779af11
liviapilotoASL <livia_piloto@asl.org>
2025-06-26 14:20:37 +0100

    theopposites

--- theOpposites.js
+++ theOpposites.js
@@ -6,3 +6,11 @@ oppositeNum(10)
 oppositeNum(-5) 
 // returns 
 5
+
+function oppositeNum(num) {
+    return -num;
+    }
+console.log(oppositeNum(10));  // Output: -10
+console.log(oppositeNum(-5));  // Output: 5
+console.log(oppositeNum(0));   // Output: 0
+console.log(oppositeNum(100)); // Output: -100
